7e8ca38fa16c2f78c7fbb27d762913d8d4912de2
meh <meh>
2018-06-15 07:14:31 +0000

    add regstration

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,7 @@
 */
 
 Route::get('/', 'WelcomeController@index');
+
+//signup
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
